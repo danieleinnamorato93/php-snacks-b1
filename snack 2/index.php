a20f5436b6caf97f5c34c2903a1effa913611659
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-04 11:52:32 +0200

    first commit

---
+++ snack 2/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+</body>
+
+</html>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <form action="index.php" method="GET">
+                <div>
+                    <input class="form-control form-control-lg" type="text" id="name" name="name" placeholder="Name"
+                        aria-label=".form-control-lg example">
+                </div>
+                <div>
+                    <input class="form-control form-control-lg" type="text" id="mail" name="mail" placeholder="mail"
+                        aria-label=".form-control-lg example">
+                </div>
+                <div>
+                    <input class="form-control form-control-lg" type="text" id="age" name="age" placeholder="age"
+                        aria-label=".form-control-lg example">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
